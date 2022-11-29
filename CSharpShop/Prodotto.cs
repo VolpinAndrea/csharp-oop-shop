ec3b7b056297742a979b88ca8e77a28e9f7e6998
VolpinAndrea <andvolpin19@gmail.com>
2022-11-29 15:05:09 +0100

    Fine base

--- CSharpShop/Prodotto.cs
+++ CSharpShop/Prodotto.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,92 @@ namespace CSharpShop
 {
     internal class Prodotto
     {
+        private int codice;
+        private string nome;
+        private string descrizione;
+        private double prezzo;
+        private double iva;
+
+        Random RandomClass = new Random();
+        /*Usate opportunamente i livelli di accesso (public, private), i costruttori, i metodi getter e setter ed eventuali altri metodi di “utilità” per fare in modo che:
+alla creazione di un nuovo prodotto il codice sia valorizzato con un numero random.*/
+        public Prodotto() 
+        {
+            this.codice = RandomClass.Next(1, 10000);
+        }
+
+        public Prodotto(string nome, string descrizione, double prezzo, double iva):this()
+        {
+            this.nome = nome;   
+            this.descrizione = descrizione;
+            this.prezzo = prezzo;
+            this.iva = iva;
+        }
+        /*Il codice prodotto sia accessibile solo in lettura
+Gli altri attributi siano accessibili sia in lettura che in scrittura*/
+        public int getCodice()
+        {
+            return this.codice;
+        }
+
+        public string getNome()
+        {
+            return this.nome;
+        }
+        public void setNome(string newNome)
+        {
+            this.nome = newNome;
+        }
+
+        public string getDescrizione()
+        {
+            return this.descrizione;
+        }
+        public void setDescrizione(string newDescrizione)
+        {
+            this.descrizione = newDescrizione;
+        }
+
+        public double getPrezzo()
+        {
+            return this.prezzo;
+        }
+        public void setPrezzo(double newPrezzo)
+        {
+            this.prezzo = newPrezzo;
+        }
+            public double getIva()
+        {
+            return this.iva;
+        }
+        public void setIva(double newIva)
+        {
+            this.iva = newIva;
+        }
+
+        /*Il prodotto esponga sia un metodo per avere il prezzo base che uno per avere il prezzo comprensivo di iva.*/
+        public double prezzoIva()
+        {
+            return this.prezzo+this.iva;
+        }
+        public string nomeEsteso() 
+        { 
+            return this.codice + this.nome;
+        }
+
+        public void stampaTutto()
+        {
+            Console.WriteLine("------------------");
+            Console.WriteLine("Codice: " + this.codice);
+            Console.WriteLine("Nome: " + this.nome);
+            Console.WriteLine("Descrizione: " + descrizione); // senza this apposta
+            Console.WriteLine("Prezzo: " + this.prezzo);
+            Console.WriteLine("IVA: " + this.iva);
+            Console.WriteLine("Prezo con IVA : " + string.Format("{0:F2}", prezzoIva())); // taglia di decimali ma trasforma in string
+            Console.WriteLine("Nome esteso: " + nomeEsteso());
+
+        }
+
+
     }
 }
--- CSharpShop/Program.cs
+++ CSharpShop/Program.cs
@@ -1,2 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+/*Nel progetto CSharpShop , creare la classe Prodotto che gestisce i prodotti del vostro shop.
+Un prodotto è caratterizzato da:
+codice (numero intero)
+nome
+descrizione
+prezzo
+iva
+Usate opportunamente i livelli di accesso (public, private), i costruttori, i metodi getter e setter ed eventuali altri metodi di “utilità” per fare in modo che:
+alla creazione di un nuovo prodotto il codice sia valorizzato con un numero random.
+Il codice prodotto sia accessibile solo in lettura
+Gli altri attributi siano accessibili sia in lettura che in scrittura
+Il prodotto esponga sia un metodo per avere il prezzo base che uno per avere il prezzo comprensivo di iva.
+Il prodotto esponga un metodo per avere il nome esteso, ottenuto concatenando codice + nome
+Testate poi i vostri oggetti Prodotto, istanziandoli e provando ad interagirci con loro per testare tutti i metodi che avete previsto e implementato.
+BONUS:
+create un metodo che restituisca il codice con un pad left di 0 per arrivare a 8 caratteri (ad esempio codice 91 diventa 00000091, mentre codice 123445567 resta come è)
+Usando una List, dichiarate una lista dei prodotti di un negozio e inseriteci al suo interno qualche prodotto che vi aspettate di trovare nel negozio. Stampate poi l’elenco dei vostri prodotti che avete previsto nel negozio.
+Buon lavoro!*/
+using CSharpShop;
+
+Prodotto pUno = new Prodotto();
+pUno.stampaTutto();
+pUno.setNome("pane");
+pUno.setDescrizione("caldo");
+pUno.setPrezzo(3.5);
+pUno.setIva(1.1);
+pUno.stampaTutto();
+
+Prodotto pDue = new Prodotto("Salmone", "Fresco", 12.45, 3.6);
+pDue.stampaTutto();
+pDue.setDescrizione("affumicato");
+pDue.setPrezzo(8.12);
+pDue.stampaTutto();
